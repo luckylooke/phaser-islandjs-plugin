bf066deffb9e10526b91ecb5e90bb97cd7044392
Ctibor Laky <luckylooke@gmail.com>
2015-08-12 11:19:06 +0000

    demo page cellautoVoronoi section

--- src/game.js
+++ src/game.js
@@ -1,17 +1,75 @@
 
-/* global Phaser */
+/* global Phaser CAVWorld */
 window.onload = function () {
-  function create(){
-      var island = this.game.plugins.add(Phaser.Plugin.Island, {
-        // custom settings
-          perlinWidth: this.game.width/2,
-          perlinHeight: this.game.height/2
-      });
-      // island.DISPLAY_COLORS.OCEAN = '#4444ff';
-      island.renderNow();
+  function preload(){
+    this.load.image('cave', 'src/assets/cave.png');
   }
+  function create(){
+    var island = this.game.plugins.add(Phaser.Plugin.Island, {
+      // custom settings
+        perlinWidth: this.game.width/2,
+        perlinHeight: this.game.height/2
+    });
+    // island.DISPLAY_COLORS.OCEAN = '#4444ff';
+    island.renderNow();
+    
+    function example_caves() {
   
-  var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-game', {create: create});
+    	var cells = island.diagram.cells,
+    	  world = new CAVWorld(cells);
+
+    	world.registerCellType('wall', {
+    		process: function (cellVoronoi) {
+    		  var neighbors = [],
+    		    neighborsIds = cellVoronoi.getNeighborIds(),
+    		    surrounding;
+    		    
+    		  for (var i = neighborsIds.length; i--; ) {
+    		    neighbors.push(cells[neighborsIds[i]]);
+    		  }
+    		  
+    			surrounding = this.countSurroundingCellsWithValue(neighbors, 'wasOpen');
+    			this.open = (this.wasOpen && surrounding >= 3) || surrounding >= 5;
+    		},
+    		reset: function () {
+    			this.wasOpen = this.open;
+    		}
+    	}, function () {
+    		//init
+    		this.open = Math.random() > 0.40;
+    	});
+    
+    	world.initialize([
+    		{ name: 'wall', distribution: 100 }
+    	]);
+    
+    	return world;
+    }
+    
+    var test = example_caves();
+    
+    var testBtnFn = function() {
+      test.step();
+      for (var i = 0; i < test.voronoiCells.length; i++) {
+        // console.log(test.voronoiCells[i].ca.open);
+        var cell = test.voronoiCells[i],
+          ctx = island.cellsLayer.ctx;
+          
+        ctx.fillStyle = cell.ca.open ? '#fff' : '#000';
+        
+        ctx.beginPath();
+        ctx.arc(cell.site.x,cell.site.y,5,0,2*Math.PI);
+        ctx.fill();
+      }
+      island.cellsLayer.dirty = true;
+    };
+    
+    var button = this.game.add.button(95, 400, 'cave', testBtnFn, this, 2, 1, 0);
+      button.fixedToCamera = true;
+      button.cameraOffset.setTo(10, 30);
+    }
+    
+  new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-game', {create: create, preload:preload});
   
   function create2(){
     var island = this.game.plugins.add(Phaser.Plugin.Island, {
@@ -27,8 +85,6 @@ window.onload = function () {
       });
   }
   
-  var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-game-debugger', {create: create2});
-  
-  
+  new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'phaser-game-debugger', {create: create2});
 };
  
